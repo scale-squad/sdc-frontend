8cb441f3842cce981d64e608be8cac85f293a5d4
mth830 <mth830@gmail.com>
2024-10-09 12:09:11 -0400

    removed star samples from App.jsx

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -7,22 +7,9 @@ const App = () => {
   let [productId, setProductId] = useState(65631);
   let [styleId, setStyleId] = useState(1);
   return (<div>
-    <StarRating rating={5} />
-    <StarRating rating={4.89} enableText={true} />
-    <StarRating rating={4.85} enableText={true} />
-    <StarRating rating={4.75} enableText={true} />
-    <StarRating rating={3.25} />
-    <StarRating rating={2.11} />
-    <StarRating rating={1.9} />
-    <StarRating rating={1.8} />
-    <StarRating rating={"1.75"} />
-    <StarRating rating={1.6} />
-    <StarRating rating={1.5} />
-    <StarRating rating={1} />
     <p>{productId}</p>
   </div>
   )
-}
-  ;
+};
 
 export default App;
