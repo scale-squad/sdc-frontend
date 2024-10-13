12eac7cbbbcb8e574bc61d9e96576b721eaaa62f
mth830 <mth830@gmail.com>
2024-10-13 00:54:53 -0400

    slider implemented

--- client/src/components/RatingsAndReviews/SizeComfort.jsx
+++ client/src/components/RatingsAndReviews/SizeComfort.jsx
@@ -3,11 +3,8 @@ import Slider from './Slider.jsx';
 const SizeComfort = ({characteristics}) => {
   const maxWidth = 270
   const {Comfort,Fit,Length,Quality} = characteristics;
-  console.log(characteristics)
   const fitSegments = ['Too Small','Perfect','Too Large'];
   const comfortSegments =['Poor','','','Perfect'];
-  // const megaSegments =new Array(10).fill().map((x,i)=>String.fromCharCode(97+i));
-  // <Slider segments={megaSegments} value={Comfort.value} title={"Mega"}/>
   return (
     <div className="size-comfort">
       <Slider segments={fitSegments} value={Fit.value} title={"Size"}/>
--- client/src/components/RatingsAndReviews/Slider.jsx
+++ client/src/components/RatingsAndReviews/Slider.jsx
@@ -1,41 +1,30 @@
 import React from 'react';
 const Slider = ({ segments, value, title, maxWidth = 270 }) => {
-  const arrowPosition = Math.floor((value-1) / 4 * maxWidth);
-  const arrowStyle={position:"relative",left:`${arrowPosition}px`,fontSize:15,width:0};
+  if (value === undefined) { return <div>Error Loading Slider</div> }
+  else if (value < 1 || value > 5) return <div>Value out of Bounds</div>
+  const spaceBetween = 5;
+  const arrowPosition = Math.floor((value - 1) / 4 * (maxWidth - 2 * spaceBetween) + spaceBetween);
+  const arrowStyle = { position: "relative", left: `${arrowPosition}px`, fontSize: 15, width: 0 };
   const downArrow = <p style={arrowStyle}>&#9660;</p>
   const spaces = segments.length - 1;
-  const spaceBetween = 5;
-  const halfSpace = Math.floor(spaceBetween / 2);
   const spaceLength = spaces * spaceBetween;
   const remainingWidth = maxWidth - spaceLength;
   const singleSegmentWidth = Math.floor(remainingWidth / segments.length);
-  const style = { 'width': `${singleSegmentWidth}px`, marginRight: halfSpace, marginLeft: halfSpace };
-  const rightStyle = { ...style, marginRight: 0 };
-  const leftStyle = { ...style, marginLeft: 0 };
-  console.log(value);
+  const tableStyle = { borderSpacing: spaceBetween, width: maxWidth };
+
   return (
     <div className="slider">
       <div className="slider-title">{title}</div>
-      <div style={{width:maxWidth}}>{downArrow}</div>
-      <table >
+      <div style={{ width: maxWidth }}>{downArrow}</div>
+      <table style={tableStyle}>
         <tr>
-      {
-        segments.map((seg, i) => {
-          let currStyle = style;
-          if (i === 0) currStyle = leftStyle;
-          else if (i === segments.length - 1) currStyle = rightStyle;
-          return <td className="slider-segment" style={currStyle}></td>
-        })
+          {
+            segments.map((seg, i) => <td style={tableStyle} className="slider-segment" ></td>)
 
-      }</tr><tr>{
-        segments.map((seg, i) => {
-          let currStyle = style;
-          if (i === 0) currStyle = leftStyle;
-          else if (i === segments.length - 1) currStyle = rightStyle;
-          return <td className="slider-label" style={currStyle}>{seg}</td>
-        })
-      }
-      </tr>
+          }</tr><tr>{
+            segments.map((seg, i) => <td cstyle={tableStyle} lassName="slider-label" >{seg}</td>)
+          }
+        </tr>
       </table>
 
     </div>
