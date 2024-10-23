c320b6578b81a2728f739bafdf6dcec457c05ab0
Omar Mendez <omar.i.mendez@gmail.com>
2024-10-23 18:25:02 -0500

    Fixed unique key error

--- client/src/components/ProductDetail/StyleSelector.jsx
+++ client/src/components/ProductDetail/StyleSelector.jsx
@@ -13,12 +13,13 @@ const StyleSelector = ({currentProductStyle, setCurrentProductStyle, productStyl
     setCurrentProductStyle(productStyles[index]);
   }
 
+  console.log(productStyles);
   return (
     <div>
       <h4>{"STYLE > "}{currentProductStyle.name}</h4>
       <div className="style-selector">
         {productStyles.map((style, index) => (
-          <div className="style-selector-image-crop">
+          <div className="style-selector-image-crop" key={style.style_id}>
             <img className="style-image" key={index} src={style.photos[0].thumbnail_url} onClick={()=>styleClicked(index)} />
           </div>
         ))}
